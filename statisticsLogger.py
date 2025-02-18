58d911e9aac518f3ce0cf742a7bcd544fcf5b434
Morgan Roux <roux.morgan@gmail.com>
2025-02-18 10:08:44 +0100

    add average time

--- statisticsLogger.py
+++ statisticsLogger.py
@@ -13,7 +13,12 @@ class StatisticsLogger:
 
     def report(self):
         print("======= REPORT =======")
-        n_tasks = np.unique(
-            np.concatenate([[t.name for t in d["tasks"]] for d in self.data])
+        unique_tasks = list(set(np.concatenate([d["tasks"] for d in self.data])))
+        # for task in unique_tasks:
+        #     print(task)
+        print(f"number of tasks : {len(unique_tasks)}")
+        times = map(
+            lambda task: task.finished_at - task.created_at,
+            filter(lambda t: t.finished_at is not None, unique_tasks),
         )
-        print(f"number of tasks : {len(n_tasks)}")
+        print(f"average time : {np.mean(list(times))}")
